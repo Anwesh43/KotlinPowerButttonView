0e791a464df25d74e310c753fa36835a2ddd0ee6
Anwesh43 <anweshthecool0@gmail.com>
2018-01-23 07:42:37 +0530

    adding Animator

--- powerbuttonview/src/main/java/ui/anwesome/com/powerbuttonview/PowerButtonView.kt
+++ powerbuttonview/src/main/java/ui/anwesome/com/powerbuttonview/PowerButtonView.kt
@@ -19,4 +19,29 @@ class PowerButtonView(ctx:Context):View(ctx) {
         }
         return true
     }
+    data class Animator(var view:PowerButtonView,var animated:Boolean = false) {
+        fun animate(updatecb:()->Unit) {
+            if(animated) {
+                updatecb()
+                try {
+                    Thread.sleep(50)
+                    view.invalidate()
+                }
+                catch(ex:Exception) {
+
+                }
+            }
+        }
+        fun start() {
+            if(!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+        fun stop() {
+            if(animated) {
+                animated = false
+            }
+        }
+    }
 }
